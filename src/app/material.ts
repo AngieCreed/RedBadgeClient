e807f47d9b1fb689daf27c2d4d2837ddfb908967
Angie Creed <angie.creed15@gmail.com>
2018-10-31 16:01:58 -0400

    navbar & welcome buttons

--- src/app/material.ts
+++ src/app/material.ts
@@ -1,8 +1,11 @@
-import { MatButtonModule } from "@angular/material";
+import {MatIconModule} from '@angular/material';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatButtonModule } from "@angular/material/button";
+import {MatMenuModule} from '@angular/material/menu';
 import { NgModule } from "@angular/core";
 
 @NgModule({
-  imports: [MatButtonModule],
-  exports: [MatButtonModule]
+  imports: [MatButtonModule, MatMenuModule, MatToolbarModule, MatIconModule],
+  exports: [MatButtonModule, MatMenuModule, MatToolbarModule, MatIconModule]
 })
 export class MaterialModule {}
