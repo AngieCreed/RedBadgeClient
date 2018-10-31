0ddb199e72a105c3503a0dcd044fa93d7c390487
Angie Creed <angie.creed15@gmail.com>
2018-10-31 09:17:02 -0400

    initial upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GoldbadgeComponent } from './goldbadge/goldbadge.component';
+import { JavascriptComponent } from './javascript/javascript.component';
+import { HtmlComponent } from './html/html.component';
+import { CssComponent } from './css/css.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    NavbarComponent,
+    GoldbadgeComponent,
+    JavascriptComponent,
+    HtmlComponent,
+    CssComponent
   ],
   imports: [
     BrowserModule,
