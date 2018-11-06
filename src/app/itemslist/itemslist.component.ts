251d610f63be5a9a927fff9e4c214aff158daf7d
Kent B Carlyle II <kentbcarlyle@gmail.com>
2018-11-06 16:45:34 -0500

    add bracket to itemslist, add animations to html comp

--- src/app/itemslist/itemslist.component.ts
+++ src/app/itemslist/itemslist.component.ts
@@ -1,27 +1,30 @@
 import { Component, Input, Output, OnInit, EventEmitter } from "@angular/core";
 import { Item } from "../shared/item.model";
-import { transition, style, animate, trigger, query, stagger } from "@angular/animations";
+import {
+  transition,
+  style,
+  animate,
+  trigger,
+  query,
+  stagger
+} from "@angular/animations";
 
 @Component({
   selector: "app-itemslist",
   templateUrl: "./itemslist.component.html",
   styleUrls: ["./itemslist.component.scss"],
   animations: [
-    trigger('fadeIn', [
-      transition('void => *', [
+    trigger("fadeIn", [
+      transition("void => *", [
         query(":enter", style({ opacity: 0 })),
 
-        query(":enter", stagger(400, [
-          animate(3000, style({ opacity: 1}))
-        ])),
-
-        
-    ]),
-    transition(':leave', [
-      animate('0.5s 1s ease-in', style({ transform: 'translateX(-100%)'}))
+        query(":enter", stagger(400, [animate(3000, style({ opacity: 1 }))]))
+      ])
+      // transition(':leave', [
+      //   animate('0.5s 1s ease-in', style({ transform: 'translateX(-100%)'}))
+      // ])
     ])
   ]
-]
 })
 export class ItemslistComponent implements OnInit {
   @Input()
