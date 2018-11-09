613f9ecbd2f8d2cf1f4cae619cd0534ae7b9659a
Benjamin Donahue <benjamindonahue@Benjamins-MacBook-Pro.local>
2018-11-09 13:58:34 -0500

    removed slash in APIURL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export let APIURL = '';
 
 switch (window.location.hostname) {
   case 'bootcampfootlocker-client.herokuapp.com':
-    APIURL = 'https://bootcampfootlocker-client.herokuapp.com/';
+    APIURL = 'https://bootcampfootlocker-client.herokuapp.com';
     break;
   default: APIURL = 'http://localhost:3000';
 }
