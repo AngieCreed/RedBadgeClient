efbac70df3bbc9ebe27884066bb0ddd357af4ab1
Benjamin Donahue <benjamindonahue@Benjamins-MacBook-Pro.local>
2018-11-09 18:45:36 -0500

    16

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ export const environment = {production: true};
 export let APIURL = '';
 
 switch (window.location.hostname) {
-  case 'bootcampfootlocker-client.herokuapp.com':
-    APIURL = 'https://bootcampfootlocker-server.herokuapp.com';
+  case 'bootcampfootlocker-client.herokuapp.com/':
+    APIURL = 'https://bootcampfootlocker-server.herokuapp.com/';
     break;
   default: APIURL = 'http://localhost:3000';
 }
