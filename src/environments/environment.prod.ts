87d6907b23138966dd2c0d2a5aeb9e78d8493d8b
Kent B Carlyle II <kentbcarlyle@gmail.com>
2018-11-14 11:53:50 -0500

    remove slash from environment case

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = { production: true };
 export let APIURL = "";
 
 switch (window.location.hostname) {
-  case "bootcampfootlocker-client.herokuapp.com/":
+  case "bootcampfootlocker-client.herokuapp.com":
     APIURL = "https://bootcampfootlocker-server.herokuapp.com/";
     break;
   default:
